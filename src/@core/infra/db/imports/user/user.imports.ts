161eb74bc15bd21de8ce9a037b5efa664c5b8be5
Leonardo Barreiros <leossb36@gmail.com>
2023-12-06 10:57:07 -0300

    fix: removing unusual imports

--- src/@core/infra/db/imports/user/user.imports.ts
+++ src/@core/infra/db/imports/user/user.imports.ts
@@ -1,21 +1,9 @@
 import { UserType, UserTypeSchema } from '@infra/db/schema/userType.schema';
-import {
-  InterestedArea,
-  InterestedAreaSchema,
-} from '@infra/db/schema/interestedArea.schema';
-import {
-  Availability,
-  AvailabilitySchema,
-} from '@infra/db/schema/availability.schema';
 import { User, UserSchema } from '@infra/db/schema/user.schema';
 import { Code, CodeSchema } from '../../schema/code.schema';
-import { PairUp, PairUpSchema } from '../../schema/pair-up.schema';
 
 export const UserFeature = [
   { name: User.name, schema: UserSchema },
   { name: UserType.name, schema: UserTypeSchema },
-  { name: InterestedArea.name, schema: InterestedAreaSchema },
-  { name: Availability.name, schema: AvailabilitySchema },
-  { name: PairUp.name, schema: PairUpSchema },
   { name: Code.name, schema: CodeSchema },
 ];
